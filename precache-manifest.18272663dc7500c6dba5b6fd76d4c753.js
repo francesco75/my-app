1cd80c09bdc9856b0114512db460a75ac63484f3
FRSCO <frscollo@hotmail.it>
2020-05-19 12:45:14 +0100

    Updates

---
+++ precache-manifest.18272663dc7500c6dba5b6fd76d4c753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3dc69a4da8037f8f4237be410ec642f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3c03e64fbee89217d038",
+    "url": "/my-app/static/css/main.c5c1d047.chunk.css"
+  },
+  {
+    "revision": "105e6b312c163bbe0fcd",
+    "url": "/my-app/static/js/2.f679d92d.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/my-app/static/js/2.f679d92d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c03e64fbee89217d038",
+    "url": "/my-app/static/js/main.bb82e21a.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
